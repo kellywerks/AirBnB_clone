799aadb5b296f428439bfc86687d9d9e826ea733
kelly becker & Elikem Adadevoh <student@epicodus.com>
2014-04-16 10:45:44 -0700

    adds basic rails functionality

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Airbnb::Application.config.secret_key_base = 'd8a1267bd95d30ff554b339435c2a32b1ea175f44d851a60c054921f53a712f6ee5efe0c1376fa4da2827e5ee5948d901c660e51767992b7fa3376ebe4fd9a01'
